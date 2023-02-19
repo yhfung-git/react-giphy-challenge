64d4253e44351cba4aabde7906a1706c29a2a934
yonghowfung <yhfung940217@gmail.com>
2023-02-19 22:59:41 +0100

    added constructor and value for input

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-  handleUpdate = (event) => this.props.searchFunction(event.target.value);
+  constructor(props) {
+    super(props);
+
+    this.state = ({
+      term: ""
+    })
+  }
+
+  handleUpdate = (event) => {
+    this.setState({
+      term: event.target.value
+    })
+
+    this.props.searchFunction(event.target.value);
+  }
 
   render() {
     return (
       <input
+        value={this.state.term}
         type="text"
         placeholder="Search..."
         className="form-control form-search"
