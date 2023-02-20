dae8535f1196a95766172c771d959a3ea510dbb1
yonghowfung <yhfung940217@gmail.com>
2023-02-20 11:48:53 +0100

    reset

---
+++ src/components/gif.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class Gif extends Component {
+  handleClick = () => {
+    if (this.props.selectedGif) {
+      this.props.selectedGif(this.props.id);
+    }
+  }
+
+  render() {
+    if (!this.props.id) {
+      return null;
+    }
+
+    const src = `https://media0.giphy.com/media/${this.props.id}/giphy.gif`;
+
+    return (
+      <img src={src} alt="gif" className='gif' onClick={this.handleClick}/>
+    )
+  }
+}
+
+export default Gif;
---
+++ src/components/search_bar.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = ({
+      term: ""
+    })
+  }
+
+  handleUpdate = (event) => {
+    this.setState({
+      term: event.target.value
+    })
+
+    this.props.searchFunction(event.target.value);
+  }
+
+  render() {
+    return (
+      <input
+        value={this.state.term}
+        type="text"
+        placeholder="Search..."
+        className="form-control form-search"
+        onChange={this.handleUpdate}
+      />
+    )
+  }
+}
+
+export default SearchBar;
